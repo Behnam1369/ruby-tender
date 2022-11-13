9c12af6cc2a770c7e93083401f17dc99ad396faf
behnam1369 <behnam.aghaali@yahoo.com>
2022-11-13 18:44:26 +0330

    Update Invitation Controller

--- app/controllers/invitation_controller.rb
+++ app/controllers/invitation_controller.rb
@@ -1,5 +1,14 @@
 class InvitationController < ApplicationController
   def show
-    render json: Invitation.last
+
+    @invitation = Invitation.select(:SubmitTime, :IdTenderInvitation, :SendTime,
+                                    :IdTender, :IdCustomerInfo).includes(:tender, :customer).find_by(UniqueKey: params[:key])
+
+    render json: @invitation,
+           include: [tender: { only: %i[Subject ClosingDate TenderNo ClosingDate Cur Note InvitationText],
+                               include: [tender_products: { only: ['IdCommodityProduct'],
+                                                            methods: %w[formula publication product offer_type] }] }, 
+                    customer: {only: %i[Title]}                                
+                    ]
   end
 end
