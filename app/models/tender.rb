8c9568fc7e7403aaffb4caeb7c90b81ae05066c4
behnam1369 <behnam.aghaali@yahoo.com>
2022-11-13 18:42:32 +0330

    Update Tender Model

--- app/models/tender.rb
+++ app/models/tender.rb
@@ -1,5 +1,6 @@
 class Tender < ApplicationRecord
   has_many :invitations, foreign_key: 'IdTender'
+  has_many :tender_products, foreign_key: 'IdTender'
 
   self.table_name = 'Tender'
   self.primary_key = 'IdTender'
