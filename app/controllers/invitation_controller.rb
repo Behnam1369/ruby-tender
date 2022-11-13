49d691ba02457c4fd8ec806c451fca0e75468413
behnam1369 <behnam.aghaali@yahoo.com>
2022-11-13 18:46:34 +0330

    Fix Rubocop errors

--- app/controllers/invitation_controller.rb
+++ app/controllers/invitation_controller.rb
@@ -1,14 +1,12 @@
 class InvitationController < ApplicationController
   def show
-
-    @invitation = Invitation.select(:SubmitTime, :IdTenderInvitation, :SendTime,
-                                    :IdTender, :IdCustomerInfo).includes(:tender, :customer).find_by(UniqueKey: params[:key])
+    @invitation = Invitation.select(:SubmitTime, :IdTenderInvitation, :SendTime, :IdTender, :IdCustomerInfo)
+      .includes(:tender, :customer).find_by(UniqueKey: params[:key])
 
     render json: @invitation,
            include: [tender: { only: %i[Subject ClosingDate TenderNo ClosingDate Cur Note InvitationText],
                                include: [tender_products: { only: ['IdCommodityProduct'],
-                                                            methods: %w[formula publication product offer_type] }] }, 
-                    customer: {only: %i[Title]}                                
-                    ]
+                                                            methods: %w[formula publication product offer_type] }] },
+                     customer: { only: %i[Title] }]
   end
 end
