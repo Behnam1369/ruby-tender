fbce77a070e6f8b716feb391123eb4f16c98f60a
behnam1369 <behnam.aghaali@yahoo.com>
2022-11-14 17:33:17 +0330

    Configure Time Zone

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,8 @@ module RubyTender
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.active_record.default_timezone = :local
+    config.time_zone = "Tehran"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
